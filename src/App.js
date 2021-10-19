952e712aec9f59f4c8faf215c4dc646891c8678d
Chidubem <kingsleychidubem042@gmail.com>
2021-10-19 14:33:14 -0700

    Finished Project

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
           {state => (
             <div
               style={{
-                backgroundColor: "red",
+                backgroundColor: "lightsteelblue",
                 width: 100,
                 height: 100,
                 margin: "auto",
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import './List.css';
 
 class List extends Component {
